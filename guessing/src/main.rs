a663e1f4750c06d3933fe15d05418af54d12e2c0
Skelde <thomas.feuilletin1@gmail.com>
2022-03-18 13:27:07 +0100

    final guessing game

--- guessing/src/main.rs
+++ guessing/src/main.rs
@@ -1,3 +1,36 @@
+use rand::Rng;
+use std::io;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+
+    println!("Bienvenue dans le guessing game !");
+
+    let rnd_num = rand::thread_rng().gen_range(1..101);
+
+    loop{
+        println!("votre nombre ?");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to readline");
+
+        println!("vous avez tapez : {}", guess);
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&rnd_num){
+            Ordering::Less => println!("Trop petit"),
+            Ordering::Greater => println!("Trop grand !"),
+            Ordering::Equal => {
+                println!("Perfecto !");
+                break;
+            }
+        }
+    }
 }
