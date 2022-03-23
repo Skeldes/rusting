e2a78b1436631273b9dc21d49a009088f2eb2686
Skelde <thomas.feuilletin1@gmail.com>
2022-03-23 13:13:12 +0100

    pattern matching is cool

--- enum_and_pattern/src/main.rs
+++ enum_and_pattern/src/main.rs
@@ -1,3 +1,43 @@
+enum IpAddrKind {
+    V4(u8,u8,u8,u8),
+    V6(String),
+}
+
+enum Message {
+    Quit,
+    Move(i32,i32),
+    Write(String),
+    ChangeColor(i32,i32,i32),
+}
+
+impl Message{
+    fn call(&self){
+        //Some stuff
+    }
+}
+
+
+
+fn show_kind(ip: IpAddrKind) {
+    match ip {
+        IpAddrKind::V4(_,_,_,_) => println!("It's  v4 !"),
+        IpAddrKind::V6(_) => println!("It's a v6 !"),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let four = IpAddrKind::V4(127,0,0,1);
+
+    let config_max = Some(3u8);
+    match config_max {
+        Some(max) => println!("The maximum value is : {}", max),
+        _ => (),
+    }
+
+    if let Some(max) = config_max {
+        println!("This work too : {}", max);
+    }
+
+    show_kind(four);
 }
