956e5773f9c59c4d56a82aa90bcf3c5e83fbba21
Skelde <thomas.feuilletin1@gmail.com>
2022-03-25 09:45:16 +0100

    begin with vector

--- collection/src/main.rs
+++ collection/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+
+    {//Vector part
+        let v: Vec<i32> = Vec::new();
+        println!("Value of v : {:?}", v);
+
+        let v = vec![1,2,3];
+        println!("Value of v : {:?}", v);
+
+        let mut v : Vec<i32> = Vec::new();
+
+        v.push(18);
+        v.push(17);
+        v.push(18);
+        v.pop();
+        println!("Value of v : {:?}", v);
+    }
 }
